9055f9cc9ad023b12a5b377db70e17469a92e440
Jon Haun <jonhaun858@gmail.com>
2020-08-01 12:09:32 -0700

    Adding some comments

--- fan_service.py
+++ fan_service.py
@@ -1,5 +1,28 @@
 """
-toml_test.py
+fan_service.py
+
+Linux Python service solution for managing the Yahboom RGB fan HAT on Raspberry Pi
+
+MIT License
+
+Copyright (c) [2020] [Jon Haun]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
 
@@ -33,7 +56,7 @@ class RGBFanManager:
     # Other Constants
     MAX_LED = 3
     WRITE_SLEEP = 0.25
-    BEHAVIOR_FILE = "behavior.toml" #"/usr/local/share/RGBHatInterface/behavior.json"
+    BEHAVIOR_FILE = "/usr/local/share/RGBHatInterface/behavior.toml"
     DEFAULT_MESSAGE = "Hostname: " + subprocess.check_output('hostname', shell = True).decode('utf-8')
     
     # speed steps (0x00 = 0%, 0x01 = 100%)
